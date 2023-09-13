b385d15b8bfdf87c647acb5ec331d33d611ddef5
nauw1010 <nauw1010@gmail.com>
2023-09-14 05:08:13 +0800

    add

---
+++ modules.py
@@ -0,0 +1,25 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        pass
+    
+    def forward(self, x):
+        pass
